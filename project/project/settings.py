bb94a805a8c1d12b15781de4edc4af1837271844
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2013-04-19 08:40:27 -0500

    updated with fix to problem using ALLOWED_HOSTS

--- project/project/settings.py
+++ project/project/settings.py
@@ -11,7 +11,7 @@ TEMPLATE_CONTEXT_PROCESSORS += (
 
 # toggle True to prove to yourself that everything works just fine in debug mode
 DEBUG = False
-TEMPLATE_DEBUG = DEBUG
+TEMPLATE_DEBUG = True
 
 ADMINS = (
     ('Your Name', 'your_email@example.com'),
@@ -33,7 +33,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost',]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
